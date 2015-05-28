4f83074afc398973a22916b9a323c420bffe6e44
franz knupfer & Jim Patt <student@epicodus.com>
2015-05-28 16:51:07 -0700

    working code and web interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,36 @@
 var wordOrder = function(phrase) {
   var phraseArray = phrase.split(" ");
   phraseArray.sort();
+  var wordArray = [];
   var finalArray = [];
-
   while (phraseArray.length !== 0) {
     var word = phraseArray.shift();
     var counter = 1;
-    finalArray.push(word);
+    // wordArray.push(word);
 
     while (word === phraseArray[0]) {
       counter += 1;
       phraseArray.shift();
     }
-    finalArray.push(counter);
+    wordArray.push(counter);
+    wordArray.push(word);
+    finalArray.push(wordArray);
+    wordArray = [];
   }
+
+  finalArray.sort();
+  finalArray.reverse();
   return finalArray;
 }
+
+$(document).ready(function() {
+  $("form#phrases").submit(function(event) {
+    var phrase = ($("input#phrase2").val());
+    var sorted = wordOrder(phrase);
+
+    $(".output").text(sorted);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,21 @@
 describe("wordOrder", function() {
   it("takes the phrase hello and returns 1", function() {
-    expect(wordOrder("hello")).to.eql(["hello", 1]);
+    expect(wordOrder("hello")).to.eql([[1, "hello"]]);
   });
 
   it("takes the phrase hello hello and returns 2", function() {
-    expect(wordOrder("hello hello")).to.eql(["hello", 2]);
+    expect(wordOrder("hello hello")).to.eql([[2, "hello"]]);
   });
 
   it("counts each instance of an individual word", function() {
-    expect(wordOrder("hello hello darling")).to.eql(["darling", 1, "hello", 2]);
+    expect(wordOrder("hello darling hello")).to.eql([[2, "hello"], [1, "darling"]]);
   });
 
   it("counts each instance of an individual word", function() {
-    expect(wordOrder("hello darling hello")).to.eql(["darling", 1, "hello", 2]);
+    expect(wordOrder("fun good cool fun fun good cool")).to.eql([[3, "fun"], [2, "good"], [2, "cool"]]);
   });
 
-  it("counts each instance of an individual word", function() {
-    expect(wordOrder("fun good cool fun fun good cool")).to.eql(["cool", 2, "fun", 3, "good", 2]);
+  it("sorts the words in a phrase based on how many times it occurs in that phrase", function() {
+    expect(wordOrder("hello darling hello")).to.eql([[2, "hello"],[1, "darling"]])
   });
 });
